b58ef2c407893e4ef5f995654a023501b0c01d95
Hidde Schultze <hidde@hidde.dev>
2024-09-28 19:20:34 +0200

    dodelijk gek

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import asset4 from "./assets/Asset 4.png";
 import asset5 from "./assets/Asset 5.png";
 import asset6 from "./assets/Asset 6.png";
 import asset7 from "./assets/Asset 7.svg";
+import asset8 from "./assets/Asset 8.png";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -15,31 +18,80 @@ export default function Home() {
           <h1>RORIDE WITH JP</h1>
         </div>
         <div className="bg-[#909DCF] leading-none p-4 rounded-xl">
-          <h2 className="text-[#4B2567]">A KILLER WORKOUT AT <a href="https://maps.app.goo.gl/DZJNTfBLEpKV8cfj6" className="underline" target="_blank">KAATSTRAAT 59 UTRECHT</a></h2>
+          <h2 className="text-[#4B2567]">
+            A KILLER WORKOUT AT{" "}
+            <a
+              href="https://maps.app.goo.gl/DZJNTfBLEpKV8cfj6"
+              className="underline"
+              target="_blank"
+            >
+              KAATSTRAAT 59 UTRECHT
+            </a>
+          </h2>
         </div>
+        <div className="bg-slate-800 leading-none p-8 rounded-lg">
+          <h3 className="mb-2">Get your credit</h3>
+          <p className="text-muted mb-4">
+            No Habbo credits allowed. Ro-ro-rogier pricing applies (€ 17,50).
+            Date T.B.D.
+          </p>
 
-[get your unique credit here]
-[get your unique credit here]
-[get your unique credit here]
-[get your unique credit here]
-
-        <div className="bg-[#98835F] mt-12 p-6 text-center sm:text-left sm:flex-row  flex-col gap-4 rounded flex items-center w-full">
-          <h3 className="mr-4 flex-shrink-0 leading-none">
-            FEATURING
-            <br />
-            MUSIC FROM
-          </h3>
-          <div className="flex gap-8 flex-wrap sm:justify-start justify-center items-center">
-            <Image alt="powerful company" src={asset6} height="30"></Image>
-
-            <Image alt="powerful company" src={asset7} height="30"></Image>
-            <Image alt="powerful company" src={asset1} height="30"></Image>
-            <Image alt="powerful company" src={asset2} height="30"></Image>
-            <Image alt="powerful company" src={asset3} height="30"></Image>
-            <Image alt="powerful company" src={asset4} height="30"></Image>
-            <Image alt="powerful company" src={asset5} height="30"></Image>
+          <div className="flex gap-2 flex-col mt-2">
+            <Input placeholder="The name of your Bike"></Input>
+            <Input placeholder="Your name"></Input>
+            <Button>Get Credit</Button>
           </div>
         </div>
+        {/* <Input placeholder=""></Input> */}
+
+        <div className="bg-[#98835F] mt-12 p-6 text-center sm:text-left sm:flex-row  flex-col gap-4 rounded-lg flex flex-wrap items-center w-auto">
+          <div className="flex">
+            <h3 className="mr-8 flex-shrink-0 leading-none">
+              FEATURING
+              <br />
+              MUSIC FROM
+            </h3>
+            <div className="flex gap-8 flex-wrap sm:justify-start justify-center items-center">
+              <Image alt="powerful company" src={asset6} height="30"></Image>
+              <Image
+                alt="powerful company"
+                className="brightness-[1000]"
+                src={asset8}
+                height="30"
+              ></Image>
+
+              <Image
+                alt="powerful company"
+                className="brightness-[1000]"
+                src={asset7}
+                height="30"
+              ></Image>
+              <Image alt="powerful company" src={asset1} height="30"></Image>
+              <Image alt="powerful company" src={asset2} height="30"></Image>
+              <Image alt="powerful company" src={asset3} height="30"></Image>
+              <Image alt="powerful company" src={asset4} height="30"></Image>
+              <Image alt="powerful company" src={asset5} height="30"></Image>
+            </div>
+          </div>
+          <div className="flex gap-4 mt-10 items-start">
+            <div className="border-black border-[0.6rem] max-w-[18rem] text-center bg-white text-2xl text-black p-5 leading-none font-[Arial] font-bold">
+              Binnen Roken Toegestaan
+            </div>
+            <div className="border-black border-[0.6rem] max-w-[18rem] text-center bg-white text-2xl text-black p-5 leading-none font-[Arial] font-bold">
+              Rookcycle is Dodelijk. Turn the Dial to 8.{" "}
+            </div>
+            <div className="border-black border-[0.6rem] max-w-[24rem] text-center bg-white text-2xl text-black p-5 leading-none font-[Arial] font-bold">
+              Sporten kan de bloedsomloop verbeteren en veroorzaakt impotentie
+            </div>
+          </div>
+        </div>
+        <a
+          className="opacity-15 underline"
+          target="_blank"
+          href="https://github.com/hiddehs/roridewithjp"
+        >
+          open source
+        </a>
       </main>
     </div>
   );
